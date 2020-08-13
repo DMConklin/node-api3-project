f0e24692ff982c494ae693ce54645376c99627d2
Darrell-Conklin <dmconklin84@gmail.com>
2020-08-12 19:06:40 -0500

    connected server.js to index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+const server = require('./server')
+
+const PORT = process.env.port || 5000
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+})
+
